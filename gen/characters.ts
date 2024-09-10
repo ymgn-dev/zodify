2882e2b5eee89adf5dafa0606e2c33504e4397c9
ymgn-dev <ymgn.dev@gmail.com>
2024-09-10 20:16:39 +0900

    fix

---
+++ gen/characters.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+// Enum type
+export const enumTypeSchema = z.enum(["ROSA", "MICO", "NITA"]);
+
+// Character skill model
+export const skillSchema = z.object({
+  id: z.string().uuid(),
+
+  // Skill name
+  name: z.string().min(2).max(10),
+
+  createdAt: z.string().datetime(),
+
+  updatedAt: z.string().datetime(),
+});
+
+// Character model
+export const characterSchema = z.object({
+  id: z.string().uuid(),
+
+  // Character name
+  name: z.string().min(2).max(10),
+
+  // power number
+  power: z.number(),
+
+  skills: z.array(skillSchema).max(2).default([]),
+
+  createdAt: z.string().datetime(),
+
+  updatedAt: z.string().datetime(),
+});
