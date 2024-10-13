ed1af3d00c7b84dc64ad485a3453898a4b7cb8cb
ymgn-dev <ymgn.dev@gmail.com>
2024-10-13 16:59:43 +0900

    fix: oneOf, anyOf check

--- src/validators/schema-property/one-or-any-of.ts
+++ src/validators/schema-property/one-or-any-of.ts
@@ -8,4 +8,9 @@ export const oneOfAnyOfSchemaPropertyValidator = z.object({
   oneOf: z.array(z.any()).optional(),
   description: z.string().optional(),
   default: z.any().array().optional(),
-})
+}).refine(
+  data => (data.anyOf?.length ?? 0) > 0 || (data.oneOf?.length ?? 0) > 0,
+  {
+    message: 'Either \'anyOf\' or \'oneOf\' must have at least one element',
+  },
+)
